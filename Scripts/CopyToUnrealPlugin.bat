db2671e3ef1f7cb7b2dd65cb05c539cb6116d47c
Jake Hart <jakejakejakejake@gmail.com>
2026-07-18 13:29:39 -0700

    Changed CopyToUnrealPlugin.bat to create symlinks instead of actually copying. Will rename the script in the next commit. The extra copies of the source files were driving me insane, since they easily became out of date from the "real" versions in Source/, and I often accidentally edited the copy instead of the original. I had to symlink each individual file instead of just linking the Source/ directory itself into the Unreal plugins directory, since Unreal requires all .cpp files to be in Private/ and all .h files to be in Public/

--- Scripts/CopyToUnrealPlugin.bat
+++ Scripts/CopyToUnrealPlugin.bat
@@ -12,17 +12,41 @@ mkdir %UNREAL_PLUGIN_OUTPUT_PATH%
 mkdir %UNREAL_RUNTIME_HEADER_PATH%
 mkdir %UNREAL_RUNTIME_SOURCE_PATH%
 
-copy %KURVEBALL_ROOT%\Source\*.h %UNREAL_RUNTIME_HEADER_PATH%
-copy %KURVEBALL_ROOT%\Source\*.inl %UNREAL_RUNTIME_HEADER_PATH%
-copy %KURVEBALL_ROOT%\Source\*.cpp %UNREAL_RUNTIME_SOURCE_PATH%
+:: Headers and inlines -> Public/
+for %%f in (Source\*.h) do (
+    if exist "%UNREAL_RUNTIME_HEADER_PATH%\%%~nxf" del "%UNREAL_RUNTIME_HEADER_PATH%\%%~nxf"
+    mklink "%UNREAL_RUNTIME_HEADER_PATH%\%%~nxf" "%CD%\%%f"
+)
 
-:: Unreal-specific wrapper types go in their own subdirectory
+for %%f in (Source\*.inl) do (
+    if exist "%UNREAL_RUNTIME_HEADER_PATH%\%%~nxf" del "%UNREAL_RUNTIME_HEADER_PATH%\%%~nxf"
+    mklink "%UNREAL_RUNTIME_HEADER_PATH%\%%~nxf" "%CD%\%%f"
+)
+
+:: Sources -> Private/
+for %%f in (Source\*.cpp) do (
+    if exist "%UNREAL_RUNTIME_SOURCE_PATH%\%%~nxf" del "%UNREAL_RUNTIME_SOURCE_PATH%\%%~nxf"
+    mklink "%UNREAL_RUNTIME_SOURCE_PATH%\%%~nxf" "%CD%\%%f"
+)
+
+:: UnrealWrapper subdirectory
 mkdir %UNREAL_RUNTIME_HEADER_PATH%\UnrealWrapper\
 mkdir %UNREAL_RUNTIME_SOURCE_PATH%\UnrealWrapper\
 
-copy %KURVEBALL_ROOT%\Source\UnrealWrapper\*.h %UNREAL_RUNTIME_HEADER_PATH%\UnrealWrapper\
-copy %KURVEBALL_ROOT%\Source\UnrealWrapper\*.inl %UNREAL_RUNTIME_HEADER_PATH%\UnrealWrapper\
-copy %KURVEBALL_ROOT%\Source\UnrealWrapper\*.cpp %UNREAL_RUNTIME_SOURCE_PATH%\UnrealWrapper\
+for %%f in (Source\UnrealWrapper\*.h) do (
+    if exist "%UNREAL_RUNTIME_HEADER_PATH%\UnrealWrapper\%%~nxf" del "%UNREAL_RUNTIME_HEADER_PATH%\UnrealWrapper\%%~nxf"
+    mklink "%UNREAL_RUNTIME_HEADER_PATH%\UnrealWrapper\%%~nxf" "%CD%\%%f"
+)
+
+for %%f in (Source\UnrealWrapper\*.inl) do (
+    if exist "%UNREAL_RUNTIME_HEADER_PATH%\UnrealWrapper\%%~nxf" del "%UNREAL_RUNTIME_HEADER_PATH%\UnrealWrapper\%%~nxf"
+    mklink "%UNREAL_RUNTIME_HEADER_PATH%\UnrealWrapper\%%~nxf" "%CD%\%%f"
+)
+
+for %%f in (Source\UnrealWrapper\*.cpp) do (
+    if exist "%UNREAL_RUNTIME_SOURCE_PATH%\UnrealWrapper\%%~nxf" del "%UNREAL_RUNTIME_SOURCE_PATH%\UnrealWrapper\%%~nxf"
+    mklink "%UNREAL_RUNTIME_SOURCE_PATH%\UnrealWrapper\%%~nxf" "%CD%\%%f"
+)
 
 :: Return to Scripts directory
 popd
